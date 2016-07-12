012c4affdb8a5b4307faf526c4ea38327557e673
TanXiao <tanxiaosysu@outlook.com>
2016-07-12 20:23:50 +0800

    Add Some Models & ViewModels

---
+++ Client/Models/Mail.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MidtermProject
+{
+    class Mail
+    {
+        public string sender { get; set; }
+        public string receiver { get; set; }
+        public string title { get; set; }
+        public string time { get; set; }
+        public string content { get; set; }
+
+        //public Mail(string sender, string receiver, string title, string time, string content)
+        //{
+        //    this.sender = sender;
+        //    this.receiver = receiver;
+        //    this.title = title;
+        //    this.time = time;
+        //    this.content = content;
+        //}
+    }
+
+}
